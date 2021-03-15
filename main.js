8ce0a6027e06ed0c0c5c6c1505bf88516cc89bfa
ishwar2009 <73827732+ishwar2009@users.noreply.github.com>
2021-03-15 17:36:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:310,
+    height:310,
+    img_format:'png',
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_image(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">'
+    })
+    console.log('ml5.version:',ml5.version);
+        
+    
+}
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded(){
+    console.log('model loaded')
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,got_result)
+}
+function got_result(error,results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results)
+    document.getElementById("object_name").innerHTML=results[0].label;
+}
+    }
